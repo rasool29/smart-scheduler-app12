7318a14cde1191e61c7752ae0f9d2d46b1cf727c
Rasool <rasooltheeditor@gmail.com>
2025-06-01 01:05:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def authenticate_google():
             client_config = st.secrets["GOOGLE_CLIENT_SECRET"]
             flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token_file:
-            token_file.write(creds.to_json())
+        with open(token_path, 'w') as token_file:
+                token_file.write(creds.to_json())
     return creds
 
 def upload_tasks_to_calendar(tasks):
