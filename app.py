ced1da9e3dbab60dd6baeb7493a6b3f2a2e41e6d
Rasool <rasooltheeditor@gmail.com>
2025-05-31 22:24:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,14 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
 def authenticate_google():
     creds = None
-    token_path = json.loads(st.secrets["GOOGLE_CLIENT_SECRET"])
-    flow = InstalledAppFlow.from_client_config(token_path, SCOPES)
+    token_path = 'token.json'
     if os.path.exists(token_path):
         creds = Credentials.from_authorized_user_file(token_path, SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            client_config = json.loads(st.secrets["GOOGLE_CLIENT_SECRET"])
-            flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open(token_path, 'w') as token_file:
             token_file.write(creds.to_json())
